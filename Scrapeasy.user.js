41354110cfb3bf22fd899add5e76f733f28ea539
oabtm <oabtm@gmx.fr>
2016-06-22 15:38:03 +0200

    Use the least precise selector when it return the same elements..

--- Scrapeasy.user.js
+++ Scrapeasy.user.js
@@ -3,6 +3,7 @@
 // ==/UserScript==
 var UI, attributeList;
 var rules = {};
+var minimumNodeListLength = 3;
 window.addEventListener("load", function() {
 	//checking that we're not in an iframe
 	if (window === window.top) {
@@ -11,29 +12,108 @@ window.addEventListener("load", function() {
 		attributeList = createAttributeList(UI);
 	}
 })
-function generate(){
-	if(!Object.getOwnPropertyNames(rules).length){
-	    alert("You haven't selected any elements!");
-	}
-	else{
-	    var script = "data:text/javascript," +
-	    `var Scrapeasy = require("Scrapeasy");
-	    var pattern = ` + JSON.stringify(rules) + `;
-	    Scrapeasy("` + window.location + `", pattern, function(err, data){
-	        if(err) throw err;
-	        console.log(data);
-	    });`
-		window.open(script, "_blank");
-	}
+
+
+function insertCss() {
+	var style = document.createElement("style");
+	style.innerHTML = css;
+	document.head.appendChild(style);
 }
+function createUI() {
+	function cBox(bClass) {
+		return '<input class="' + bClass + ' not-nhover" type="checkbox">'
+	}
+	var UI = document.createElement("div");
+	UI.className = "not-nhover not-nhover-main";
+	UI.innerHTML = `<a href='#' class='not-nhover'>
+	<h1 class='not-nhover wsnw show-attr'>Less</h1></a>
+	<a href='#' class='not-nhover'><h1 class='not-nhover wsnw show-attr'>Start</h1></a><br>
+	<p class='not-nhover'>Click on Start then move your mouse to select elements, <br>press ALT to lock on a group of elements.</p>
+	<br><br> Multiple css selectors only?` + cBox("non-unique")
+	+ "<br> Same index only?" + cBox("same-index")
+	+ "<br> Same index from last only?" + cBox("same-index-end") + "<hr>";
+	UI.querySelectorAll("a")[0].addEventListener("click", moreOrLess);
+	UI.querySelectorAll("a")[1].addEventListener("click", start);
+	document.body.insertBefore(UI, document.body.firstChild);
+	UI.querySelector(".non-unique").checked = true;
+	return UI;
+}
+
+
+function createAttributeList(UI) {
+	var attributeList = document.createElement("div");
+	attributeList.className = "not-nhover";
+	UI.appendChild(attributeList);
+	return attributeList;
+}
+function updateAttributeList(lasthover) {
+	attributeList.innerHTML = "";
+	if (!lasthover.className.match("not-nhover")) {
+		if (lasthover.innerHTML.length) {
+			showAttribute({
+				name: "text",
+				value: lasthover.innerText
+			}, attributeList);
+			showAttribute({
+				name: "html",
+				value: lasthover.innerHTML
+			}, attributeList);
+		}
+		if (lasthover.value) {
+			showAttribute({
+				name: "value",
+				value: lasthover.value
+			}, attributeList);
+		}
+		[].slice.call(lasthover.attributes).filter(function(a) {
+			showAttribute(a, attributeList);
+		});
+	}
+	[].slice.call(document.querySelectorAll(".show-attr-btn")).filter(function(b) {
+		b.addEventListener("click", showMore);
+	})
+}
+function showAttribute(toShow, list) {
+	var attribute = document.createElement("div");
+	var showMore = document.createElement('span');
+	showMore.setAttribute("class", "not-nhover show-attr show-attr-btn");
+	showMore.innerText = "+";
+	var name = document.createElement('span');
+	name.setAttribute("class", "not-nhover show-attr name");
+	name.innerText = toShow.name;
+	var value = document.createElement('input');
+	value.setAttribute("class", "not-nhover");
+	value.setAttribute("readonly", "true");
+	value.setAttribute("placeholder", toShow.value);
+	attribute.appendChild(showMore);
+	attribute.appendChild(name);
+	attribute.appendChild(value);
+	list.appendChild(attribute);
+}
+
+
+////////// Main buttons onclick functions //////////
+//Minimize/Maximize the User Interface
+function moreOrLess(e) {
+	if (this.innerText === "Less") {
+		this.querySelector("h1").innerText = "More";
+		UI.className += " minimizeUI";
+	} else {
+		this.querySelector("h1").innerText = "Less";
+		UI.className = UI.className.replace(" minimizeUI", "");
+	}
+	UI.scrollTop = 0;
+	e.preventDefault();
+}
+//Highlight hovered elements and display their attributes in attributeList
 function start(e) {
-	var lastSelector = "";
+	var lastSelector = ":not(*)";
 	var lockOnElement = false;
 	this.querySelector("h1").innerText = "Generate script";
 	this.removeEventListener("click", start);
 	this.addEventListener("click", generate);
 	document.addEventListener("keydown", function(e) {
-		if (e.which === 17) {
+		if (e.which === 18) {
 			if (lockOnElement) {
 				lockOnElement = false;
 			} else {
@@ -49,13 +129,13 @@ function start(e) {
 		if (!lockOnElement) {
 			var hovered = document.elementFromPoint(e.clientX, e.clientY);
 			if (lasthover) {
-				lasthover.className = lasthover.className.replace(" current-hover", "");
+				lasthover.setAttribute("class", lasthover.getAttribute("class").replace(" current-hover", ""))
 			}
-			hovered.className += " current-hover";
+			hovered.setAttribute("class", hovered.getAttribute("class") + " current-hover")
 			if (lasthover !== hovered) {
 				lasthover = hovered;
 				updateAttributeList(hovered);
-				removeHighlighting();
+				unhighlight();
 				preciseSelector(unique.checked, sameIndex.checked, sameIndexEnd.checked);
 			}
 		}
@@ -63,98 +143,49 @@ function start(e) {
 	e.preventDefault();
 }
 
-function insertCss() {
-	var newCss = document.createElement("style");
-	newCss.innerHTML = scrapCss;
-	document.head.appendChild(newCss);
-}
 
-function cBox(bClass) {
-	return '<input class="' + bClass + ' not-nhover" type="checkbox">'
-}
-
-function createUI() {
-	var UI = document.createElement("div");
-	UI.className = "not-nhover not-nhover-main";
-	UI.innerHTML = `<a href='#' class='not-nhover'>
-    <h1 class='not-nhover wsnw show-attr'>Less</h1></a>
-	<a href='#' class='not-nhover'>
-    <h1 class='not-nhover wsnw show-attr'>Start</h1></a>
-    <br> Multiple css selectors only?` + cBox("non-unique") +
-	"<br> Same index only?" + cBox("same-index") +
-	"<br> Same index from last only?" + cBox("same-index-end") + "<hr>";
-	UI.querySelectorAll("a")[0].addEventListener("click", less);
-	UI.querySelectorAll("a")[1].addEventListener("click", start);
-	document.body.insertBefore(UI, document.body.firstChild);
-	UI.querySelector(".non-unique").checked = true;
-	return UI;
-}
-function less(e){
-	if(this.innerText === "Less"){
-		this.querySelector("h1").innerText = "More";
-		UI.className += " minimizeUI";
-	}
-	else{
-		this.querySelector("h1").innerText = "Less";
-		UI.className = UI.className.replace(" minimizeUI", "");
-	}
-	UI.scrollTop = 0;
-	e.preventDefault();
-}
-
-function createAttributeList(UI) {
-	var attributeList = document.createElement("div");
-	attributeList.className = "not-nhover";
-	UI.appendChild(attributeList);
-	return attributeList;
-}
-
-function showAttribute(attr, list) {
-	var newAttr = document.createElement("div");
-	var showBtn = document.createElement('span');
-	showBtn.setAttribute("class", "not-nhover show-attr show-attr-btn");
-	showBtn.innerHTML = "+";
-	var name = document.createElement('span');
-	name.setAttribute("class", "not-nhover show-attr name");
-	name.innerHTML = attr.name;
-	var val = document.createElement('input');
-	val.setAttribute("class", "not-nhover");
-	val.setAttribute("readonly", "true");
-	val.setAttribute("placeholder", attr.value);
-	newAttr.appendChild(showBtn);
-	newAttr.appendChild(name);
-	newAttr.appendChild(val);
-	list.appendChild(newAttr);
-}
-
-function updateAttributeList(lasthover) {
-	attributeList.innerHTML = "";
-	if (!lasthover.className.match("not-nhover")) {
-		if (lasthover.innerText.length) {
-			showAttribute({
-				name: "Text",
-				value: lasthover.innerText
-			}, attributeList);
+////////// Script generation and rules saving //////////
+// Save a new rule in the object "rules" when an user click on the SAVE button created by showMore()
+function save() {
+	var as = this.parentNode.querySelector('input[type="text"]').value;
+	if (as.indexOf("[n]") === -1) {
+		alert('"As:" must follow this pattern: array[n] or array[n].property !');
+	} else {
+		if (typeof rules[lastSelector] === "undefined") {
+			rules[lastSelector] = [];
 		}
-		if (lasthover.value) {
-			showAttribute({
-				name: "value",
-				value: lasthover.value
-			}, attributeList);
+		var max = 0;
+		if (parseInt(this.parentNode.querySelector(".stop-at").value) !== document.querySelectorAll(".nhover").length) {
+			max = parseInt(this.parentNode.querySelector(".stop-at").value);
 		}
-		for (var t = 0; t < lasthover.attributes.length; t++) {
-			showAttribute(lasthover.attributes[t], attributeList);
-		}
-		[].slice.call(lasthover.attributes).filter(function(a) {
-			showAttribute(a,attributeList);
+		rules[lastSelector].push({
+			attribute: this.parentNode.parentNode.querySelector('.name').innerText,
+			as: as,
+			function: (this.parentNode.querySelector('textarea') || {
+				value: "return x;"
+			}).value,
+			offset: {
+				from: parseInt(this.parentNode.querySelector(".start-at").value),
+				to: max
+			}
 		});
 	}
-	var showBtn = document.querySelectorAll(".show-attr-btn");
-	for (var s = 0; s < showBtn.length; s++) {
-		showBtn[s].addEventListener("click", showMore);
+	return false;
+}
+//Generate a node.js script and open it in a new tab
+function generate() {
+	if (!Object.getOwnPropertyNames(rules).length) {
+		alert("You haven't selected any elements!");
+	} else {
+		var script = `var Scrapeasy = require("./Scrapeasy");\nvar pattern = `
+		+ JSON.stringify(rules, null, 8) + ';\nScrapeasy("' + window.location
+		+ '", pattern, function(err, data){\n	if(err) throw err;\n	console.log(data);\n});';
+		window.open("data:text/javascript,	" + encodeURIComponent(script), "_blank");
 	}
 }
 
+
+////////// Creating elements used in showMore() //////////
 function fxArea() {
 	var area = document.createElement("textarea");
 	area.setAttribute("class", "not-nhover");
@@ -172,7 +203,6 @@ function fxArea() {
 		}
 	});
 }
-
 function numberInput(min, max, iClass) {
 	var input = document.createElement("input");
 	input.setAttribute("type", "number");
@@ -181,30 +211,7 @@ function numberInput(min, max, iClass) {
 	input.setAttribute("class", iClass);
 	return input;
 }
-
-function save(){
-	var as = this.parentNode.querySelector('input[type="text"]').value;
-	if(as.indexOf("[n]") === -1){
-		alert('"As:" must follow this pattern: array[n] or array[n].property !');
-	}
-	else{
-		if(typeof rules[lastSelector] === "undefined"){
-			rules[lastSelector] = [];
-		}
-		var max = -1;
-		if(parseInt(this.parentNode.querySelector(".stop-at").value) !== document.querySelectorAll(".nhover").length){
-			max = parseInt(this.parentNode.querySelector(".stop-at").value);
-		}
-		rules[lastSelector].push({
-				attribute: this.parentNode.parentNode.querySelector('.name').innerText,
-				as:	as,
-				function: (this.parentNode.querySelector('textarea') || {value: "return x;"}).value,
-				offset:{from: parseInt(this.parentNode.querySelector(".start-at").value), to: max}
-		});
-	}
-	return false;
-}
-
+//Create all the input elements needed by the user to create a new rule.
 function showMore() {
 	if (this.innerHTML === "+") {
 		this.innerHTML = "-";
@@ -235,6 +242,7 @@ function showMore() {
 		inp.appendChild(saveButton);
 		this.parentNode.appendChild(inp);
 		inp.querySelector(".show-fx").addEventListener("click", fxArea)
+
 		function checkOffset(e) {
 			// We make sure that startAt value isn't bigger than stopAt and vice versa
 			if (this.className === "stop-at") {
@@ -261,12 +269,13 @@ function showMore() {
 	}
 }
 
-function removeHighlighting() {
+
+////////// Highlighting functions //////////
+function unhighlight() {
 	[].slice.call(document.querySelectorAll(".nhover")).filter(function(e) {
 		e.className = e.className.replace("nhover ", "");
 	});
 }
-
 function highlight() {
 	var rel = document.querySelectorAll(lastSelector);
 	for (var r = 0; r < rel.length; r++) {
@@ -274,8 +283,10 @@ function highlight() {
 	}
 }
 
-/*text nodes count as a child element in JS but not in CSS,
-in order to get an accurate index for an elemnt, we can use this function to ignore text nodes*/
+
+////////// DOM manipulations functions //////////
+/*text nodes count as  child elements in JS but not in CSS,
+in order to get an accurate index for an element, we need to use this function to ignore text nodes*/
 function removeTextNodes(list) {
 	var temp = [];
 	[].slice.call(list).filter(function(n) {
@@ -285,74 +296,88 @@ function removeTextNodes(list) {
 	})
 	return temp;
 }
-
 //handling browser compatibility for getIndex()
 var prev = document.body.previousElementSibling ?
 	'previousElementSibling' : 'previousSibling';
 
-function getIndex(n) {
+function getIndex(e) {
 	var i = 1;
-	while (n = n[prev]) {
+	while (e = e[prev]) {
 		i++;
 	}
 	return i;
 }
-
-function preciseSelector(nonUnique, sameIndex, sameIndexFromEnd) {
+/*Find the most precise CSS selector based on the settings of the user,
+and highlight the elements that matches it. */
+function preciseSelector(multiple, sameIndex, sameIndexFromEnd) {
 	var es = [];
 	var h = document.querySelectorAll(":hover");
 	for (var hl = 0; hl < h.length; hl++) {
-		es.push(relatedSelector(h[hl], nonUnique));
+		es.push(relatedSelector(h[hl], multiple));
 	}
 	var indexSelector = "";
-	if (h.length > 1) {
 		var current = h[h.length - 1];
 		if (sameIndexFromEnd && current.parentNode) {
 			/*if the parentNode doesn't have any others children,
 			using an index become useless, so we're checking for the parent of the parent*/
 			if (removeTextNodes(current.parentNode.childNodes).length > 1) {
 				var index = removeTextNodes(current.parentNode.childNodes).length - getIndex(current) + 1;
-				indexSelector += ":nth-last-child(" + index + ")";
-
-			} else if (current.parentNode.parentNode && es.length > 1 &&
-				removeTextNodes(current.parentNode.parentNode.childNodes).length > 1) {
+				if(document.querySelectorAll(es.join(">") + ":nth-last-child(" + index + ")").length > minimumNodeListLength){
+					indexSelector += ":nth-last-child(" + index + ")";
+				}
+			} else if (current.parentNode.parentNode && es.length > 1 && removeTextNodes(current.parentNode.parentNode.childNodes).length > 1) {
 				var index = current.parentNode.parentNode.childNodes.length - getIndex(current.parentNode) + 1;
-				es[es.length - 2] += ":nth-last-child(" + index + ") ";
+				var esCopy = es.slice(0);
+				esCopy[esCopy.length - 2] += ":nth-last-child(" + index + ") ";
+				if(document.querySelectorAll(esCopy.join(">")).length > minimumNodeListLength){
+					es[es.length - 2] += ":nth-last-child(" + index + ") ";
+				}
 			}
-
 		}
 		if (sameIndex) {
 			if (removeTextNodes(current.parentNode.childNodes).length > 1) {
-				indexSelector += ":nth-child(" + getIndex(current) + ")";
-			} else if (current.parentNode.parentNode && es.length > 1 &&
-				removeTextNodes(current.parentNode.parentNode.childNodes).length > 1) {
-				es[es.length - 2] += ":nth-last-child(" + getIndex(current.parentNode) + ") ";
+				if(document.querySelectorAll(es.join(">") + indexSelector + ":nth-child(" + getIndex(current) + ")").length > minimumNodeListLength)
+				{
+					indexSelector += ":nth-child(" + getIndex(current) + ")";
+				}
+			} else if (current.parentNode.parentNode && es.length > 1 && removeTextNodes(current.parentNode.parentNode.childNodes).length > 1) {
+				var esCopy = es.slice(0);
+				esCopy[esCopy.length - 2] += ":nth-last-child(" + getIndex(current.parentNode) + ") ";
+				if(document.querySelectorAll(esCopy.join(">")).length > minimumNodeListLength){
+					es[es.length - 2] += ":nth-last-child(" + getIndex(current.parentNode) + ") ";
+				}
 			}
 		}
-	}
 	lastSelector = es.join(">") + indexSelector;
 	highlight();
 }
 
-/*return a css selector made of the tagName + the classes of an elemnt,
-if nonUnique is true, we remove the classes used by less than 3 elements */
-function relatedSelector(element, nonUnique) {
+function relatedSelector(element, multiple) {
 	var classes = element.className.split(" "),
-		empty = classes.lastIndexOf(""),
-		tag = element.tagName;
+	    empty = classes.lastIndexOf(""),
+	    tag = element.tagName;
 	while (empty !== -1) {
-		classes.splice(empty, 1);
-		empty = classes.lastIndexOf("");
+	    classes.splice(empty, 1);
+	    empty = classes.lastIndexOf("");
 	}
+	var selectors = []
 	for (var c = classes.length - 1; c > -1; c--) {
-		var selector = tag + "." + classes.slice(0, c + 1).join(".");
-		if (!nonUnique || document.querySelectorAll(selector).length > 3) {
-			return selector;
-		}
+	    var selector = tag + "." + classes.slice(0, c + 1).join(".");
+	    var l = document.querySelectorAll(selector).length;
+	    if (!multiple || l > minimumNodeListLength) {
+	        selectors.push([selector, l])
+	    }
+	}
+	if(selectors.length){
+	    for(var y = selectors.length - 1; y > -1; y--){
+	        if(selectors[y][1] === selectors[0][1]){
+	            return selectors[y][0]
+	        }
+	    }
 	}
 	return tag;
 }
-var scrapCss = `
+var css = `
 /*////////////////// Showing attributes //////////////////*/
 
 .show-attr {
@@ -435,6 +460,7 @@ var scrapCss = `
     border: 2px solid;
     height: 20em;
 	min-width: 340px;
+	width: 340px;
     resize: both;
     overflow: auto;
     left: 0;
